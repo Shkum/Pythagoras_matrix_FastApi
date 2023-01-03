1d0c644dbedce4dab384c4171241ffe24f84114f
Error <shkum79@ukr.net>
2023-01-03 19:23:39 +0100

    Update

--- app.py
+++ app.py
@@ -11,16 +11,7 @@ from fastapi.templating import Jinja2Templates
 
 app = FastAPI()
 
-# app.mount("/static", StaticFiles(directory="static"), name="static")
-
-
-app.mount(
-    "/static",
-    StaticFiles(directory=Path(__file__).parent.parent.absolute() / "static"),
-    name="static",
-)
-
-
+app.mount("/static", StaticFiles(directory="static"), name="static")
 
 templates = Jinja2Templates(directory="templates")
 
